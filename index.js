f6b7f53ae49469b63840ae80231e155d75d83e9b
Adnan Sayed <adnan.sayed.official@gmail.com>
2021-04-12 02:17:19 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+let main = document.getElementById('main');
+
+main.remove();
+
+
+let newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+newHeader.innerHTML = 'Adnan is the champion';
+
+body.appendChild(newHeader);
