d5bef59c72e752e952744c91c437ad9e62554852
Omar Nachar <omarnachar2@gmail.com>
2021-04-14 09:39:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+let ul = document.querySelector('body');
+ul.removeChild(ul.querySelector('main'));
+let newHeader = document.createElement('h1');
+newHeader.setAttribute("id","victory");
+newHeader.innerHTML = "YOUR - NAME is the champion";
