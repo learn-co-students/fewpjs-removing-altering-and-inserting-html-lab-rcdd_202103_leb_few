7713b440a01aabf27c33c6287dd464c1fa3a1af7
Mohamad  Al Sabaa <mohamadsabaa3@gmail.com>
2021-04-22 15:36:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const body = document.body;
+const main = document.getElementById('main')
+main.remove();
+
+const newHeader = document.createElement("h1")
+newHeader.setAttribute('id','victory')
+newHeader.innerHTML = `Mohamad is the champion`;
+body.appendChild(newHeader);
